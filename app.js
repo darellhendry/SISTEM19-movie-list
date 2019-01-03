8e7257fac7432bb14be50726a4d320a144301096
darell <1darellhendry@gmail.com>
2019-01-03 20:36:07 +0700

    darell

--- app.js
+++ app.js
@@ -18,10 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/users', usersRouter);
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/', indexRouter);
 app.get('*', (req, res) => {
  res.sendFile(path.join(__dirname+'/client/build/index.html'));
