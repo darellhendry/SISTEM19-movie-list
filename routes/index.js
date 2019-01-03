2131064e422bec864146ce59339deec940f48392
darell <1darellhendry@gmail.com>
2019-01-03 20:23:12 +0700

    set url ro react

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
 module.exports = router;
